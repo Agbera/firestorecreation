dd84942e2980f1c12722cb6c464fbfdc87975ee3
Agbera <126923646+Agbera@users.noreply.github.com>
2023-03-31 18:30:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 provider "google"{}
 resource "google_firestore_database""database" {
   project="olagt-380915"
-  name=(default)
+  name="(default)"
   location_id="northamerica-northeast1"
   type="FIRESTORE_NATIVE"
 }
