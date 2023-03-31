7c80edfffd603f2601f4247807c3ce6f8e8ce6d0
Agbera <126923646+Agbera@users.noreply.github.com>
2023-03-30 20:33:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 #CREATE A FIRESTORE NATIVE DATABASE
 provider "google"{}
 resource "google_firestore_database""database" {
-  project="agbera-ola-380101"
+  project="olagt-380915"
   name="my-firestore-database"
   location_id="US"
   type="FIRESTORE_NATIVE"
